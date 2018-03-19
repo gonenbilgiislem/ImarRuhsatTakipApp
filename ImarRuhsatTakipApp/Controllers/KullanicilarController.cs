9e9678eaf24fb6115fe5100f7856bae0966ae438
Barış Özen <bilgiislem@balikesirgonen.bel.tr>
2018-03-19 13:59:30 +0300

    Proje dosyası ekle.

---
+++ ImarRuhsatTakipApp/Controllers/KullanicilarController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ImarRuhsatTakipApp.Models;
+
+namespace ImarRuhsatTakipApp.Controllers
+{
+    public class KullanicilarController : Controller
+    {
+        private ImarRuhsatTakipAppDb2Entities db = new ImarRuhsatTakipAppDb2Entities();
+
+        // GET: Kullanicilar
+        public ActionResult Index()
+        {
+            return View(db.Kullanicilar.ToList());
+        }
+
+        // GET: Kullanicilar/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Kullanicilar kullanicilar = db.Kullanicilar.Find(id);
+            if (kullanicilar == null)
+            {
+                return HttpNotFound();
+            }
+            return View(kullanicilar);
+        }
+
+        // GET: Kullanicilar/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Kullanicilar/Create
+        // Aşırı gönderim saldırılarından korunmak için, lütfen bağlamak istediğiniz belirli özellikleri etkinleştirin, 
+        // daha fazla bilgi için https://go.microsoft.com/fwlink/?LinkId=317598 sayfasına bakın.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Kullanici_Id,Kullanici_Ad_Soyad,Sifre,Tc")] Kullanicilar kullanicilar)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Kullanicilar.Add(kullanicilar);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(kullanicilar);
+        }
+
+        // GET: Kullanicilar/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Kullanicilar kullanicilar = db.Kullanicilar.Find(id);
+            if (kullanicilar == null)
+            {
+                return HttpNotFound();
+            }
+            return View(kullanicilar);
+        }
+
+        // POST: Kullanicilar/Edit/5
+        // Aşırı gönderim saldırılarından korunmak için, lütfen bağlamak istediğiniz belirli özellikleri etkinleştirin, 
+        // daha fazla bilgi için https://go.microsoft.com/fwlink/?LinkId=317598 sayfasına bakın.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Kullanici_Id,Kullanici_Ad_Soyad,Sifre,Tc")] Kullanicilar kullanicilar)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(kullanicilar).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(kullanicilar);
+        }
+
+        // GET: Kullanicilar/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Kullanicilar kullanicilar = db.Kullanicilar.Find(id);
+            if (kullanicilar == null)
+            {
+                return HttpNotFound();
+            }
+            return View(kullanicilar);
+        }
+
+        // POST: Kullanicilar/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Kullanicilar kullanicilar = db.Kullanicilar.Find(id);
+            db.Kullanicilar.Remove(kullanicilar);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ ImarRuhsatTakipApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ImarRuhsatTakipApp.MvcApplication" Language="C#" %>
